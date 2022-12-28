d7ae0c04fa9b2892529845445f580ffa3580cef1
Hozefa Indorewala <hozefa26@users.noreply.github.com>
2022-12-28 12:57:22 +0100

    Add example project

---
+++ IntegrationExample/IntegrationExample/IntegrationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntegrationExampleApp.swift
+//  IntegrationExample
+//
+//  Created by Hozefa Indorewala on 28.12.22.
+//
+
+import SwiftUI
+
+@main
+struct IntegrationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
