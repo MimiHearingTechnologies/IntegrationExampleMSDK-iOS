8be53f57d01c3c44f0ccee7381b5cae451f65514
Hozefa Indorewala <hozefa26@users.noreply.github.com>
2023-01-25 17:52:47 +0530

    Remove Generated files

--- IntegrationExample/Generated/AppSecrets.generated.swift
+++
@@ -1,5 +0,0 @@
-// Generated using Sourcery 1.9.2 — https://github.com/krzysztofzablocki/Sourcery
-// DO NOT EDIT
-struct AppSecrets {
-  static let mimiClientSecret="87fda96f5aa4bb38d6c9e8ec37ef3f7671c9787ce01c6102fc43858a924bca14"
-}
